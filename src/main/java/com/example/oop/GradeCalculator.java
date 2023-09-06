d17824c821686b052bb561af74712ec8d06254dc
dev22 <ysoli36512@gmail.com>
2023-09-06 17:25:12 +0900

    feat: 일급 컬렉션으로 변경하기 , 협력을 설계

--- src/main/java/com/example/oop/GradeCalculator.java
+++ src/main/java/com/example/oop/GradeCalculator.java
@@ -3,10 +3,12 @@ package com.example.oop;
 import java.util.List;
 
 public class GradeCalculator {
-    private final List<Course> courses;
+
+    private final Courses courses;
+
 
     public GradeCalculator(List<Course> courses) {
-        this.courses = courses;
+        this.courses = new Courses(courses);
     }
 
     /**
@@ -16,16 +18,12 @@ public class GradeCalculator {
      */
     public double calculateGrade() {
         //(학점수*교과목 평점)의 합계
-        double multipliedCreditAndCourseGrade = 0;
+        double multipliedCreditAndCourseGrade =  courses.multiplyCreditAndCourseGrade();
 
-        for(Course course: courses){
-          multipliedCreditAndCourseGrade +=  course.multiplyCreditAndCourseGrade();
-        }
 
         //수강신청 총학점 수
-        int totalCompletedCredit = courses.stream()
-                .mapToInt(Course::getCredit)
-                .sum();
+        int totalCompletedCredit = courses.calculateTotalCompletedCredit();
+
 
         return multipliedCreditAndCourseGrade / totalCompletedCredit;
     }
